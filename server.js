fb2c0d2d639c85a1bbd68a4e1d914d06f01eeb72
katiescruggs <katie.e.scruggs@gmail.com>
2018-02-12 10:40:56 -0700

    Add endpoint /api/v1/items to GET all items

--- server.js
+++ server.js
@@ -13,3 +13,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(app.get('port'), () => {
   console.log(`Garage Bin running on ${app.get('port')}`);
 });
+
+app.get('/api/v1/items', (request, response) => {
+  database('items').select()
+    .then(items => {
+      return response.status(200).json({ results: items });
+    })
+    .catch(error => {
+      return response.status(500).json({ error });
+    })
+});
