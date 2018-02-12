f2502680935a35d9e3cbe40a5dc81c12c309aa83
katiescruggs <katie.e.scruggs@gmail.com>
2018-02-12 11:08:51 -0700

    Test client routes

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -8,3 +8,47 @@ const server = require('../server');
 const knex = require('../db/knex');
 
 chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+  it('should return the homepage', () => {
+    return chai.request(server)
+      .get('/')
+      .then(response => {
+        response.should.have.status(200);
+        response.should.be.html;
+      })
+      .catch(error => {
+        throw error;
+      })
+  });
+
+  it('should return 404 for a route that does not exist', () => {
+    return chai.request(server)
+      .get('/doesnotexist')
+      .then(response => {
+        throw response;
+      })
+      .catch(error => {
+        error.should.have.status(404);
+      })
+  })
+});
+
+describe('API Routes', () => {
+  beforeEach(done => {
+    knex.seed.run()
+      .then(() => done());
+  });
+
+  describe('GET /api/v1/items', () => {
+
+  });
+
+  describe('POST /api/v1/items', () => {
+
+  });
+
+  describe('GET /api/v1/items/:name', () => {
+
+  });
+});
