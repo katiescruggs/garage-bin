69682453795471983cf275dbe3c7cf3d6cab093b
katiescruggs <katie.e.scruggs@gmail.com>
2018-02-12 11:19:38 -0700

    Test GET API routes

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -41,7 +41,25 @@ describe('API Routes', () => {
   });
 
   describe('GET /api/v1/items', () => {
-
+    it('should return all of the items', () => {
+      return chai.request(server)
+        .get('/api/v1/items')
+        .then(response => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.results.should.be.a('array');
+          response.body.results.length.should.equal(2);
+          response.body.results[0].should.have.property('id');
+          response.body.results[0].should.have.property('name');
+          response.body.results[0].should.have.property('reason');
+          response.body.results[0].should.have.property('cleanliness');
+          response.body.results[0].should.have.property('created_at');
+          response.body.results[0].should.have.property('updated_at');
+        })
+        .catch(error => {
+          throw error;
+        })
+    });
   });
 
   describe('POST /api/v1/items', () => {
@@ -49,6 +67,49 @@ describe('API Routes', () => {
   });
 
   describe('GET /api/v1/items/:name', () => {
+    it('should return one item with name that matches request param', () => {
+      return chai.request(server)
+        .get('/api/v1/items/dresser')
+        .then(response => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('object');
+          response.body.should.have.property('id');
+          response.body.should.have.property('name');
+          response.body.should.have.property('reason');
+          response.body.should.have.property('cleanliness');
+          response.body.should.have.property('created_at');
+          response.body.should.have.property('updated_at');
+        })
+        .catch(error => {
+          throw error;
+        })
+    });
 
+    it('should provide error message if item does not exist', () => {
+      return chai.request(server)
+        .get('/api/v1/items/doesnotexist')
+        .then(response => {
+          throw response;
+        })
+        .catch(error => {
+          error.should.have.status(404);
+          error.response.should.be.json;
+          error.response.body.should.be.a('object');
+          error.response.body.should.have.property('error');
+          error.response.body.error.should.equal('No item with name doesnotexist found.');
+        })
+    });
+
+    it('should return 404 if URL is mistyped', () => {
+      return chai.request(server)
+        .get('/api/v1/item/dresser')
+        .then(response => {
+          throw response;
+        })
+        .catch(error => {
+          error.should.have.status(404);
+        })
+    });
   });
 });
