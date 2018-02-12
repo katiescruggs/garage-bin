625427e313db5d9d3fa304896605df18c0c83a09
katiescruggs <katie.e.scruggs@gmail.com>
2018-02-12 10:32:29 -0700

    Prevent default and reset inputs on submit button click

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,3 +1,14 @@
+const resetInputs = () => {
+  $('#name-input').val('');
+  $('#reason-input').val('');
+  $('#cleanliness-select').val('Sparkling');
+}
+
 $('#garage-img').on('click', () => {
   $('#garage-img').slideUp(5000);
+});
+
+$('#submit-btn').on('click', (e) => {
+  e.preventDefault();
+  resetInputs();
 });
